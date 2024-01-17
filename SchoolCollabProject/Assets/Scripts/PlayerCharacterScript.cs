174beb02cf462208c1ef7629645240da620dd09f
DevNatrix <natrixfn@gmail.com>
2024-01-17 13:48:21 +0100

    Revert "Movement Updated + wall collision"
    This reverts commit a0552653d0912b5f1891e4009934b13aed1d22aa.

--- SchoolCollabProject/Assets/Scripts/PlayerCharacterScript.cs
+++ SchoolCollabProject/Assets/Scripts/PlayerCharacterScript.cs
@@ -8,13 +8,7 @@ public class PlayerCharacterScript : MonoBehaviour
     [Header("Character Variables")]
     public float speed = 5f;
     public Rigidbody2D rb2d;
-    public SpriteRenderer spriteRenderer;
-    private Transform direction;
-
-    private void Start()
-    {
-        
-    }
+    private Vector2 direction;
 
     private void Update()
     {
@@ -28,27 +22,7 @@ public class PlayerCharacterScript : MonoBehaviour
 
     void Move()
     {
-        if(spriteRenderer.flipX == false)
-        {
-            transform.position += transform.right * speed * Time.deltaTime;
-        }
-        else if(spriteRenderer.flipX == true)
-        {
-            transform.position += -transform.right * speed * Time.deltaTime;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("RightWall"))
-        {
-            spriteRenderer.flipX = true;
-        }
-
-        if(collision.gameObject.CompareTag("LeftWall"))
-        {
-            spriteRenderer.flipX = false;
-        }
+        transform.position += transform.right * speed * Time.deltaTime;
     }
 
 }
