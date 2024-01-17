c5c4cde9c23207e49d7b6a2d7128d423156c9aa7
DevNatrix <natrixfn@gmail.com>
2024-01-17 13:57:38 +0100

    Movement Changes After Wall collision + sprite flip
    Movement Changes After Wall collision + sprite flip

--- SchoolCollabProject/Assets/Scripts/PlayerCharacterScript.cs
+++ SchoolCollabProject/Assets/Scripts/PlayerCharacterScript.cs
@@ -8,7 +8,7 @@ public class PlayerCharacterScript : MonoBehaviour
     [Header("Character Variables")]
     public float speed = 5f;
     public Rigidbody2D rb2d;
-    private Vector2 direction;
+    public SpriteRenderer spriteRenderer;
 
     private void Update()
     {
@@ -22,7 +22,41 @@ public class PlayerCharacterScript : MonoBehaviour
 
     void Move()
     {
-        transform.position += transform.right * speed * Time.deltaTime;
+        if(spriteRenderer.flipX == false)
+        {
+            transform.position += transform.right * speed * Time.fixedDeltaTime;
+        }
+
+        if (spriteRenderer.flipX == transform)
+        {
+            transform.position += -transform.right * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    void FlipSprite()
+    {
+        if(spriteRenderer.flipX == true)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if(spriteRenderer.flipX == false)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("RightWall"))
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if(collision.gameObject.CompareTag("LeftWall"))
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
 }
